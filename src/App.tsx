cb9fbb0cc98c359da014c34950966cf4472691b5
Karan Mann <karan.mann@outlook.com>
2022-05-15 17:39:43 +0200

    onClick and onChange Events

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css';
+import Button from './components/Button';
+import Input from './components/Input';
 
 function App() {
   return (
     <div className="App">
-      <p>Learn TypeScript</p>
+      <p>Learn TypeScript - Events</p>
+      <Button handleClick={(event, id) => console.log('Button Clicked', event, id)} />
+      <Input value='' handleChange={event => console.log(event)}/>
     </div>
   );
 }
 
 export default App;
+1

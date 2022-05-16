aedde30b42c7b1ee28709bbe7b49e8ceba9b7289
Karan Mann <karan@nodon.se>
2022-05-16 19:45:30 +0200

    Descriminated Union method below

--- src/components/useReducer/Counter.tsx
+++ src/components/useReducer/Counter.tsx
@@ -3,20 +3,30 @@ import { useReducer } from "react";
 const initialState = { count: 0 };
 
 type CounterState = {
-  count: number
+  count: number;
+};
+
+
+// Descriminated Union method below
+type UpdateAction = {
+  type: "increment" | "decrement";
+  payload: number;
+};
+
+type ResetAction = {
+  type: 'reset'
 }
 
-type CounterAction = {
-  type: string,
-  payload: number
-}
+type CounterAction = UpdateAction | ResetAction;
 
 const reducer = (state: CounterState, action: CounterAction) => {
   switch (action.type) {
     case "increment":
-      return { count: state.count + action.payload };
+      return { count: state.count + action.payload};
     case "decrement":
-      return { count: state.count - action.payload };
+      return { count: state.count - action.payload};
+    case "reset":
+      return initialState;
     default:
       return state;
   }
@@ -33,6 +43,7 @@ export const Counter = () => {
       <button onClick={() => dispatch({ type: "decrement", payload: 10 })}>
         Decrement 10
       </button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
     </>
   );
 };
